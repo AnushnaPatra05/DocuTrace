c6aa1a594c26ed6894e0a6a0bc3f962774ef90d5
Priyanshu Maity <priyanshu.maity2006@gmail.com>
2026-06-11 02:35:46 +0530

    Bug Fix Token

--- apps/server/src/utils/token.ts
+++ apps/server/src/utils/token.ts
@@ -1,27 +1,46 @@
-import jwt from 'jsonwebtoken'
+import jwt, { Secret, SignOptions } from 'jsonwebtoken'
 import { env } from '../config/env'
 
-export const generateAccessToken = (userId: string, role: string): string => {
-  return jwt.sign({ userId, role }, env.JWT_ACCESS_SECRET, {
-    expiresIn: env.JWT_ACCESS_EXPIRES,
-  })
+export const generateAccessToken = (
+  userId: string,
+  role: string
+): string => {
+  return jwt.sign(
+    { userId, role },
+    env.JWT_ACCESS_SECRET as Secret,
+    {
+      expiresIn: env.JWT_ACCESS_EXPIRES as SignOptions['expiresIn'],
+    }
+  )
 }
 
-export const generateRefreshToken = (userId: string): string => {
-  return jwt.sign({ userId }, env.JWT_REFRESH_SECRET, {
-    expiresIn: env.JWT_REFRESH_EXPIRES,
-  })
+export const generateRefreshToken = (
+  userId: string
+): string => {
+  return jwt.sign(
+    { userId },
+    env.JWT_REFRESH_SECRET as Secret,
+    {
+      expiresIn: env.JWT_REFRESH_EXPIRES as SignOptions['expiresIn'],
+    }
+  )
 }
 
 export const verifyAccessToken = (token: string) => {
-  return jwt.verify(token, env.JWT_ACCESS_SECRET) as {
+  return jwt.verify(
+    token,
+    env.JWT_ACCESS_SECRET as Secret
+  ) as {
     userId: string
     role: string
   }
 }
 
 export const verifyRefreshToken = (token: string) => {
-  return jwt.verify(token, env.JWT_REFRESH_SECRET) as {
+  return jwt.verify(
+    token,
+    env.JWT_REFRESH_SECRET as Secret
+  ) as {
     userId: string
   }
 }
